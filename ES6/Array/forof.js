afb722278218a54230154ed627354bee9905a207
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-03-09 20:46:09 +0900

    Array
    for of - 순회하기

---
+++ ES6/Array/forof.js
@@ -0,0 +1,11 @@
+var data = [1, 2, undefined, NaN, null, ""];
+
+for(let value of data) {
+    console.log(value);
+}
+
+var str = "hello world!";
+
+for(let value of str) {
+    console.log(value);
+}
