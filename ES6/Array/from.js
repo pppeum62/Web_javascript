c7647556c44d23a453acbf664d9d53dd0d7bf7b6
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-03-11 21:18:36 +0900

    Array
    ES6 from method

---
+++ ES6/Array/from.js
@@ -0,0 +1,12 @@
+// ES6 From method
+
+function addMark() {
+    let newArray = Array.from(arguments);
+    let newData = newArray.map(function(value) {
+        return value + "!";
+    });
+
+    console.log(newData);
+}
+
+addMark(1, 2, 3, 4, 5);
