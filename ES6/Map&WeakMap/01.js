2917fad4bc787e5576b27af11bee0461b759ce58
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-03-20 00:32:15 +0900

    Map&WeakMap

---
+++ ES6/Map&WeakMap/01.js
@@ -0,0 +1,23 @@
+// map & weakmap
+// Array -> set, weakset
+// Object -> map, weakmap
+
+// map은 key/value
+
+let wm = new WeakMap();
+let myfun = function(){};
+
+wm.set(myfun, 0);
+
+console.log(wm);
+
+let count=0;
+for(let i = 0; i < 10; i++) {
+    count = wm.get(myfun);
+    count++;
+    wm.set(myfun, count);
+}
+
+console.log(wm.get(myfun));
+myfun = null;
+console.log(wm.has(myfun));
---
+++ ES6/Map&WeakMap/02.js
@@ -0,0 +1,20 @@
+// WeakMap 활용
+
+const wm = new WeakMap();
+
+function Area(height, width) {
+    wm.set(this, { height, width });
+}
+
+Area.prototype.getArea = function() {
+    const { height, width } = wm.get(this);
+    return height * width;
+}
+
+let myarea = new Area(10, 20);
+
+console.log(myarea.getArea());
+
+myarea = null;
+
+console.log(wm.has(myarea));
