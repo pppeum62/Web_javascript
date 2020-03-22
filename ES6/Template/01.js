ca1e1abe839f19bed3cb1bf07699cd2b51893405
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-03-22 21:57:17 +0900

    Template

---
+++ ES6/Template/01.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        name : 'coffee-bean',
+        order : true,
+        items : ['americano', 'milk', 'green-tea']
+    },
+    {
+        name : 'starbucks',
+        order : false,
+    },
+    {
+        name : 'coffee-king',
+        order : true,
+        items : ['americano', 'latte']
+    },
+]
+
+// Tagged template literals
+function fn(tags, name, items) {
+    console.log(tags);
+    if(typeof items === "undefined") {
+        items = "<span style='color:red'>주문 가능한 상품이 없습니다";
+    }
+
+    return (tags[0] + name + tags[1] + items);
+}
+
+data.forEach((v) => {
+
+    let template = fn`<div>welcome ${v.name}!!</div>
+                        <h2>주문가능항목</h2><div>${v.items}</div>`;
+    document.querySelector("#message").innerHTML += template;
+
+});
